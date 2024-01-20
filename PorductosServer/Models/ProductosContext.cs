c7bf866f947e646ec7b2942273a3da50a3e15426
Daniel Perez <daniel.perezh@usantoto.edu.co>
2024-01-19 22:09:30 -0500

    Primer commit

---
+++ PorductosServer/Models/ProductosContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PorductosServer.Models
+{
+    public class ProductosContext : DbContext
+    {
+        public ProductosContext(DbContextOptions<ProductosContext> options) : base(options) 
+        {
+        }
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Producto>().HasIndex(c => c.Nombre).IsUnique();
+        }
+    }
+}
